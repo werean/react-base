e1a40b47fa4770b56d47cd42cb8a62f27163f277
Lucas Assis <lucasassis0507@gmail.com>
2025-02-14 01:56:52 -0300

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Login from "./pages/Login/Index";
+import GlobalStyles from "./styles/GlobalStyles";
+function App() {
+  return (
+    <>
+      <Login />
+      <GlobalStyles />
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/Login/Index.jsx
@@ -0,0 +1,19 @@
+import { Title, Teste } from "../../styles/styled";
+import { Container } from "../../styles/GlobalStyles";
+export default function Login() {
+  return (
+    <Container>
+      <Title>
+        <h1>Login</h1>
+        <small>Hello</small>
+      </Title>
+      <Teste>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident pariatur sunt vero?
+          Beatae, rem reiciendis fugit
+        </p>
+      </Teste>
+      <button>teste</button>
+    </Container>
+  );
+}
