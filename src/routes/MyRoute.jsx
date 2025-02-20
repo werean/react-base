f63022f5efc1ebc106dea5c1720fe8aab26b55bb
Lucas Assis <lucasassis0507@gmail.com>
2025-02-20 17:45:01 -0300

    fixed MyRoute

--- src/routes/MyRoute.jsx
+++ src/routes/MyRoute.jsx
@@ -1,12 +1,12 @@
-import { Route, Navigate } from "react-router-dom";
+import {  Navigate } from "react-router-dom";
 import PropTypes from "prop-types";
 
-export default function MyRoute({ element, isClosed, ...rest }) {
+export default function MyRoute({ element, isClosed }) {
   const isLoggedIn = true;
   if (isClosed && !isLoggedIn) {
-    return <Navigate to={{ pathname: "/login", state: { prevPath: rest.location.pathname } }} />;
+    return <Navigate to="/login" state={{ prevPath: location.pathname }} />;
   }
-  return <Route {...rest} component={element} />;
+  return element;
 }
 
 MyRoute.defaultProps = {
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,4 +1,4 @@
-import { Routes } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Page404 from "../components/Page404";
 import Login from "../pages/Login/Index";
 import MyRoute from "./MyRoute";
@@ -6,8 +6,8 @@ import MyRoute from "./MyRoute";
 export default function Routers() {
   return (
     <Routes>
-      <MyRoute path="/" element={<Login />} />
-      <MyRoute path="*" element={<Page404 />} />
+      <Route path="/" element={<MyRoute element={<Login />} />} />
+      <Route path="*" element={<Page404 />} />
     </Routes>
   );
 }
