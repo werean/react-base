f950402e9fcde58a0937b13c829f8686b464313d
Lucas Assis <lucasassis0507@gmail.com>
2025-02-26 17:24:41 -0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,12 @@ import Login from "./pages/Login/Index";
 import GlobalStyles from "./styles/GlobalStyles";
 import { BrowserRouter } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
-import Header from "./components/Header";
-import Routes from "./routes";
+//import Header from "./components/Header";
+//import Routes from "./routes";
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Login />
       <GlobalStyles />
       <ToastContainer autoClose={3000} className="toast-container" />
